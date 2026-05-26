04f3a310743ee8e3a9218154afb315de71d9c648
Pablo <148821043+pablobmm@users.noreply.github.com>
2026-05-26 14:24:39 -0300

    chore: remove flask-mail integration from app.py

--- backend/apps/app.py
+++ backend/apps/app.py
@@ -2,22 +2,13 @@ import os
 from flask import Flask
 from flask_cors import CORS
 from flasgger import Swagger
-from apps.extensions import db_serv, mail 
+from apps.extensions import db_serv 
 
 app = Flask(__name__)
 
 CORS(app)
 
 IS_PRODUCTION = os.environ.get("RENDER", "False") == "true"
-
-# CONFIGURAÇÕES DE E-MAIL DINÂMICAS
-app.config['MAIL_SERVER'] = os.environ.get("MAIL_SERVER")
-app.config['MAIL_PORT'] = int(os.environ.get("MAIL_PORT", 587))
-app.config['MAIL_USE_TLS'] = os.environ.get("MAIL_USE_TLS", "True") == "True"
-app.config['MAIL_USE_SSL'] = os.environ.get("MAIL_USE_SSL", "False") == "True"
-app.config['MAIL_USERNAME'] = os.environ.get("MAIL_USERNAME")
-app.config['MAIL_PASSWORD'] = os.environ.get("MAIL_PASSWORD")
-app.config['MAIL_DEFAULT_SENDER'] = os.environ.get("MAIL_USERNAME")
 
 # CONFIGURAÇÃO DO BANCO DE DADOS 
 DB_USER = os.environ.get("MYSQL_USER_APP") if IS_PRODUCTION else os.environ.get("MYSQL_USER_APP", "root")
